fcbb7ac30db98bbd9084b8e28eb4aa4ab5fd9672
ds-mansi <mansi.gurnani@dotsquares.com>
2023-04-05 19:23:33 +0530

    header footer

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -5,7 +5,7 @@ type props = {
 };
 // props call
 const Footer = (props: any) => {
-  console.log(props, "links");
+  console.log(props, "abc");
   //   console.log("copyright",props.copy.copyright)
   React.useEffect(() => {
     document.body.setAttribute("id", "body");
@@ -14,30 +14,37 @@ const Footer = (props: any) => {
     (document.getElementById("body") as HTMLInputElement).classList.toggle("");
   };
 
-  const footh = props?.footer?.map((foo: any) => {
-    // console.log(foo.footerHeading,"foo")
-    return <h3>{foo?.footerHeading}</h3>;
+  const footh1 = props?.footer?.footerCta1?.map((foo: any) => {
+    console.log(foo.label, "foo");
+    return <h3>{foo?.label}</h3>;
+  });
+  const footh2 = props?.footer?.footerCta2?.map((foo1: any) => {
+    console.log(foo1.label, "foo");
+    return <h3>{foo1?.label}</h3>;
+  });
+  const footh3 = props?.footer?.footerCta3?.map((foo2: any) => {
+    console.log(foo2.label, "foo");
+    return <h3>{foo2?.label}</h3>;
   });
 
-  const footd = props?.footer?.map((foo: any) => {
-    foo?.footerCta?.map((res: any) => {
-      console.log(res.label, "res");
-      return (
-        <ul>
-          <li>
-            <a href={res.link}>{res.label}</a>
-          </li>
-        </ul>
-      );
-    });
-  });
-
-  //   console.log(props._site.c_footerRights,"hvsajdgvb")
+  console.log(props.footer.footerHeading, "hvsajdgvb");
 
   return (
     <>
-      <div className="flex">{footh}</div>
-      <div >{footd}</div>
+      <div className="flex">
+        <div>
+          <h3>{props?.footer?.footerHeading}</h3>
+          <p>{footh1}</p>
+        </div>
+        <div>
+          <h3>{props?.footer?.footerHeading2}</h3>
+          <p>{footh2}</p>
+        </div>
+        <div>
+          <h3>{props?.footer?.footerHeading3}</h3>
+          <p>{footh3}</p>
+        </div>
+      </div>
     </>
   );
 };
